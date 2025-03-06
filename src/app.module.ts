a07bb88bdfdfc77daa04fafd5899c0692919dc10
Wad-Laptop <iamwad@gmail.com>
2025-03-06 21:51:18 +0700

    รันคำสั่ง nest g mo shared/global-helper เพื่อ generate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { UtilityModule } from './shared/utility/utility.module';
+import { GlobalHelperModule } from './shared/global-helper/global-helper.module';
 
 @Module({
-  imports: [ProductModule, UtilityModule],
+  imports: [ProductModule, UtilityModule, GlobalHelperModule],
   controllers: [AppController],
   providers: [AppService],
 })
