b7975e1d55c5bea6d0242a36ab158a5961a75aac
Wad-Laptop <iamwad@gmail.com>
2025-03-06 21:12:12 +0700

    รัน nest g mo shared/utility เพื่อสร้างโมดูล shared/utility ในโฟลเดอร์ shared โดยมีโครงสร้างดังนี้

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UtilityModule } from './shared/utility/utility.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, UtilityModule],
   controllers: [AppController],
   providers: [AppService],
 })
