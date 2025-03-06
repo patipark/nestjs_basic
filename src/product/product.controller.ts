6bcaae2660efb415c06b3b31f62bdb48583e21e8
Wad-Laptop <iamwad@gmail.com>
2025-03-06 21:11:34 +0700

    ทดสอบ return ค่าจาก product controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  @Get('/') //http://localhost:3000/product
+  getProducts(): any[] {
+    return [
+      {
+        id: 1,
+        name: 'Product 1',
+        price: 100,
+      },
+      {
+        id: 2,
+        name: 'Product 2',
+        price: 200,
+      },
+    ];
+  }
+}
