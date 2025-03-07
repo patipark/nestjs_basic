7753d11126b3fe79beb7c556d2e7cbae9c45d622
Wad-Laptop <iamwad@gmail.com>
2025-03-07 21:00:14 +0700

    รันคำสั่ง nest g res customer เพิ่อสร้าง CRUD resource structor ของ customer

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { UtilityModule } from './shared/utility/utility.module';
 import { GlobalHelperModule } from './shared/global-helper/global-helper.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [ProductModule, UtilityModule, GlobalHelperModule],
+  imports: [ProductModule, UtilityModule, GlobalHelperModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
