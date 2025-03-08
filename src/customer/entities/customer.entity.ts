1345fc06a926bb49f4f6aa08385dfe848e80bf14
Patipark Sopha <iamwad@gmail.com>
2025-03-08 23:06:26 +0700

    create model ของ Customer โดยมีฟิลด์ดังนี้ - id (PK , Auto Incremen , Int) #### ฟิวด์ไ่ม่ต้องกำหนด ใน model จะสร้างให้อัตโนมัติ - name - email - isActive

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,1 +1,23 @@
-export class Customer {}
+import { Column, Model, Table } from 'sequelize-typescript';
+
+@Table({
+  tableName: 'customers',
+  timestamps: false,
+  paranoid: false,
+})
+export class Customer extends Model {
+  //   @Column({
+  //     primaryKey: true,
+  //     autoIncrement: true,
+  //   })
+  //   id: number;
+
+  @Column
+  name: string;
+
+  @Column
+  email: string;
+
+  @Column
+  isActive: boolean;
+}
