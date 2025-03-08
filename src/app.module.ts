b6f4eb2a18b9113a194ed94eb47a08463e712d6d
Patipark Sopha <iamwad@gmail.com>
2025-03-08 20:53:20 +0700

    add ConfigModule ที่ App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { ProductModule } from './product/product.module';
 import { UtilityModule } from './shared/utility/utility.module';
 import { GlobalHelperModule } from './shared/global-helper/global-helper.module';
 import { CustomerModule } from './customer/customer.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [ProductModule, UtilityModule, GlobalHelperModule, CustomerModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ProductModule,
+    UtilityModule,
+    GlobalHelperModule,
+    CustomerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
