fcdd423f4c3dfeae2a4fd29fa95acf18801ae2d5
Patipark Sopha <iamwad@gmail.com>
2025-03-08 22:24:50 +0700

    config sequelize module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,20 @@ import { UtilityModule } from './shared/utility/utility.module';
 import { GlobalHelperModule } from './shared/global-helper/global-helper.module';
 import { CustomerModule } from './customer/customer.module';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mariadb', // 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '3306'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
     ProductModule,
     UtilityModule,
     GlobalHelperModule,
