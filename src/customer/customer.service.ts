59e4f822a03e8f5860d2c821ceed4bf7cf3e1812
Wad-Laptop <iamwad@gmail.com>
2025-03-09 22:19:02 +0700

    Inject the Customer model into the CustomerService class at the constructor

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -2,9 +2,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { InjectModel } from '@nestjs/sequelize';
+import { Customer } from './entities/customer.entity';
 
 @Injectable()
 export class CustomerService {
+  // Inject the Customer model into the CustomerService class at the constructor
+  constructor(
+    @InjectModel(Customer)
+    private readonly customerModel: typeof Customer,
+  ) {}
+
   create(createCustomerDto: CreateCustomerDto) {
     return 'This action adds a new customer';
   }
