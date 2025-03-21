cdb261932f6a5f9144373aba0151ce28815aa99d
Patipark Sopha <iamwad@gmail.com>
2025-03-21 16:25:05 +0700

    disable eslint

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -17,6 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   // eslint-disable-next-line @typescript-eslint/require-await
   async validate(payload: any) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     return { user_id: payload.user_id };
   }
 }
